4d200402d3412d0fce9b64b6e1958ab3e1b959bc
Gavin Quirk <gavinquirk@gmail.com>
2019-07-03 19:15:33 -0700

    connected redux to screens

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+
+import PlaceList from '../../components/PlaceList/PlaceList';
 
 class FindPlaceScreen extends Component {
-    render () {
-        return (
-            <View>
-                <Text>On FindPlaceScreen</Text>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View>
+        <Text>On FindPlaceScreen</Text>
+        <PlaceList places={this.props.places} />
+      </View>
+    );
+  }
 }
 
-export default FindPlaceScreen;
+mapStateToProps = state => {
+  return {
+    places: state.places.places
+  };
+};
+
+export default connect(mapStateToProps)(FindPlaceScreen);
--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+
+import PlaceInput from '../../components/PlaceInput/PlaceInput';
+import { addPlace } from '../../store/actions/index';
 
 class SharePlaceScreen extends Component {
-    render () {
-        return (
-            <View>
-                <Text>On SharePlaceScreen</Text>
-            </View>
-        );
-    }
+  placeAddedHandler = placeName => {
+    this.props.onAddPlace(placeName);
+  };
+
+  render() {
+    return (
+      <View>
+        <Text>On SharePlaceScreen</Text>
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+      </View>
+    );
+  }
 }
 
-export default SharePlaceScreen;
+mapDispatchToProps = dispatch => {
+  return {
+    onAddPlace: placeName => dispatch(addPlace(placeName))
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(SharePlaceScreen);
