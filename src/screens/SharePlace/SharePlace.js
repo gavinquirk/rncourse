75f52821f058cb220e7f295366e95206e93480a2
Gavin Quirk <gavinquirk@gmail.com>
2019-07-08 13:00:10 -0700

    side drawer and menu button

--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -4,8 +4,24 @@ import { connect } from 'react-redux';
 
 import PlaceInput from '../../components/PlaceInput/PlaceInput';
 import { addPlace } from '../../store/actions/index';
+// import console = require('console');
 
 class SharePlaceScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
+  }
+
+  onNavigatorEvent = event => {
+    if (event.type === 'NavBarButtonPress') {
+      if (event.id === 'sideDrawerToggle') {
+        this.props.navigator.toggleDrawer({
+          side: 'left'
+        });
+      }
+    }
+  };
+
   placeAddedHandler = placeName => {
     this.props.onAddPlace(placeName);
   };
