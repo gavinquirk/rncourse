5cd974c65487a230a9e135536cfad49180a08419
Gavin Quirk <gavinquirk@gmail.com>
2019-07-08 15:01:07 -0700

    added text inputs to auth page

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
 
 class AuthScreen extends Component {
-    loginHandler = () => {
-        startMainTabs();
-    }
+  loginHandler = () => {
+    startMainTabs();
+  };
 
-    render () {
-        return (
-            <View>
-                <Text>Auth Screen</Text>
-                <Button title="Login" onPress={this.loginHandler}/>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Please Log In</Text>
+        <Button title='Switch to Login' />
+        <TextInput placeholder='Your E-mail Address' />
+        <TextInput placeholder='Password' />
+        <TextInput placeholder='Confirm Password' />
+        <Button title='Submit' onPress={this.loginHandler} />
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    borderColor: 'red',
+    borderWidth: 1
+  }
+});
 
 export default AuthScreen;
