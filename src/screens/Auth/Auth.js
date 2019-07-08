0e4dce5b1d2c47c901388e94dd26aae524381c13
Gavin Quirk <gavinquirk@gmail.com>
2019-07-08 15:24:15 -0700

    custom input component and styling

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
+import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
 
 class AuthScreen extends Component {
   loginHandler = () => {
@@ -13,9 +14,11 @@ class AuthScreen extends Component {
       <View style={styles.container}>
         <Text>Please Log In</Text>
         <Button title='Switch to Login' />
-        <TextInput placeholder='Your E-mail Address' />
-        <TextInput placeholder='Password' />
-        <TextInput placeholder='Confirm Password' />
+        <View style={styles.inputContainer}>
+          <DefaultInput placeholder='Your E-mail Address' />
+          <DefaultInput placeholder='Password' />
+          <DefaultInput placeholder='Confirm Password' />
+        </View>
         <Button title='Submit' onPress={this.loginHandler} />
       </View>
     );
@@ -24,8 +27,12 @@ class AuthScreen extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    borderColor: 'red',
-    borderWidth: 1
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  inputContainer: {
+    width: '80%'
   }
 });
 
