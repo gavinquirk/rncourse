38a105025fff45c7b8bc97b2692c201763050b61
Gavin Quirk <gavinquirk@gmail.com>
2019-07-10 14:36:36 -0700

    fade in animation for place list

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -13,7 +13,8 @@ import PlaceList from '../../components/PlaceList/PlaceList';
 class FindPlaceScreen extends Component {
   state = {
     placesLoaded: false,
-    removeAnim: new Animated.Value(1)
+    removeAnim: new Animated.Value(1),
+    placesAnim: new Animated.Value(0)
   };
 
   static navigatorStyle = {
@@ -35,12 +36,25 @@ class FindPlaceScreen extends Component {
     }
   };
 
+  placesLoadedHandler = () => {
+    Animated.timing(this.state.placesAnim, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true
+    }).start();
+  };
+
   placesSearchHandler = () => {
     Animated.timing(this.state.removeAnim, {
       toValue: 0,
       duration: 500,
       useNativeDriver: true
-    }).start();
+    }).start(() => {
+      this.setState({
+        placesLoaded: true
+      });
+      this.placesLoadedHandler();
+    });
   };
 
   itemSelectedHandler = key => {
@@ -82,10 +96,16 @@ class FindPlaceScreen extends Component {
 
     if (this.state.placesLoaded) {
       content = (
-        <PlaceList
-          places={this.props.places}
-          onItemSelected={this.itemSelectedHandler}
-        />
+        <Animated.View
+          style={{
+            opacity: this.state.placesAnim
+          }}
+        >
+          <PlaceList
+            places={this.props.places}
+            onItemSelected={this.itemSelectedHandler}
+          />
+        </Animated.View>
       );
     }
 
