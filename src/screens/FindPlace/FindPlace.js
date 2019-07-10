4c090c91bd28c28fc9802a4a35c8ccb03f41005e
Gavin Quirk <gavinquirk@gmail.com>
2019-07-10 14:19:17 -0700

    find places button in FindPlace component

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 
 import PlaceList from '../../components/PlaceList/PlaceList';
 
 class FindPlaceScreen extends Component {
+  state = {
+    placesLoaded: false
+  };
+
   static navigatorStyle = {
     navBarButtonColor: 'orange'
   };
@@ -24,6 +28,12 @@ class FindPlaceScreen extends Component {
     }
   };
 
+  placesSearchHandler = () => {
+    this.setState({
+      placesLoaded: true
+    });
+  };
+
   itemSelectedHandler = key => {
     const selPlace = this.props.places.find(place => {
       return place.key === key;
@@ -39,17 +49,49 @@ class FindPlaceScreen extends Component {
   };
 
   render() {
-    return (
-      <View>
-        <Text>On FindPlaceScreen</Text>
+    let content = (
+      <TouchableOpacity onPress={this.placesSearchHandler}>
+        <View style={styles.searchButton}>
+          <Text style={styles.searchButtonText}>Find Places</Text>
+        </View>
+      </TouchableOpacity>
+    );
+
+    if (this.state.placesLoaded) {
+      content = (
         <PlaceList
           places={this.props.places}
           onItemSelected={this.itemSelectedHandler}
         />
+      );
+    }
+
+    return (
+      <View style={this.state.placesLoaded ? null : styles.buttonContainer}>
+        {content}
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  searchButton: {
+    borderColor: 'orange',
+    borderWidth: 3,
+    borderRadius: 50,
+    padding: 20
+  },
+  searchButtonText: {
+    color: 'orange',
+    fontWeight: 'bold',
+    fontSize: 26
+  }
+});
 
 mapStateToProps = state => {
   return {
