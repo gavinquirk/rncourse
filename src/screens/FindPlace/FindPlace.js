c1125470bbe4c23f367dcf6e8416d01b3d20b873
Gavin Quirk <gavinquirk@gmail.com>
2019-07-10 14:31:25 -0700

    animation for find places button

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Animated
+} from 'react-native';
 import { connect } from 'react-redux';
 
 import PlaceList from '../../components/PlaceList/PlaceList';
 
 class FindPlaceScreen extends Component {
   state = {
-    placesLoaded: false
+    placesLoaded: false,
+    removeAnim: new Animated.Value(1)
   };
 
   static navigatorStyle = {
@@ -29,9 +36,11 @@ class FindPlaceScreen extends Component {
   };
 
   placesSearchHandler = () => {
-    this.setState({
-      placesLoaded: true
-    });
+    Animated.timing(this.state.removeAnim, {
+      toValue: 0,
+      duration: 500,
+      useNativeDriver: true
+    }).start();
   };
 
   itemSelectedHandler = key => {
@@ -50,11 +59,25 @@ class FindPlaceScreen extends Component {
 
   render() {
     let content = (
-      <TouchableOpacity onPress={this.placesSearchHandler}>
-        <View style={styles.searchButton}>
-          <Text style={styles.searchButtonText}>Find Places</Text>
-        </View>
-      </TouchableOpacity>
+      <Animated.View
+        style={{
+          opacity: this.state.removeAnim,
+          transform: [
+            {
+              scale: this.state.removeAnim.interpolate({
+                inputRange: [0, 1],
+                outputRange: [12, 1]
+              })
+            }
+          ]
+        }}
+      >
+        <TouchableOpacity onPress={this.placesSearchHandler}>
+          <View style={styles.searchButton}>
+            <Text style={styles.searchButtonText}>Find Places</Text>
+          </View>
+        </TouchableOpacity>
+      </Animated.View>
     );
 
     if (this.state.placesLoaded) {
