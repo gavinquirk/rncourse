cfce5fe628ed4000f12b48e9e7e159a8caa78ee7
Gavin Quirk <gavinquirk@gmail.com>
2019-07-25 17:45:40 -0700

    animate to marker on map press

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -24,6 +24,13 @@ class PickLocation extends Component {
 
   pickLocationHandler = event => {
     const coords = event.nativeEvent.coordinate;
+
+    this.map.animateToRegion({
+      ...this.state.focusedLocation,
+      latitude: coords.latitude,
+      longitude: coords.longitude
+    });
+
     this.setState(prevState => {
       return {
         focusedLocation: {
@@ -47,9 +54,9 @@ class PickLocation extends Component {
       <View style={styles.container}>
         <MapView
           initialRegion={this.state.focusedLocation}
-          region={this.state.focusedLocation}
           style={styles.map}
           onPress={this.pickLocationHandler}
+          ref={ref => (this.map = ref)}
         >
           {marker}
         </MapView>
